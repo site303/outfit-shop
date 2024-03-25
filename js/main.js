0d60005033b815f4a9d726ea3c76fbb61266860d
site303 <buranuranuragan@mail.ru>
2024-03-25 16:26:46 +0300

    change search box

--- js/main.js
+++ js/main.js
@@ -11,61 +11,6 @@ function selectCategory(category) {
 
 //слайдер 
 
-// const slider = document.querySelector('.slider');
-// const slides = document.querySelector('.slides');
-// const slideWidth = slider.clientWidth;
-// let currentIndex = 0;
-// let intervalId;
-
-
-// function nextSlide() {
-//   if (currentIndex < slides.children.length - 1) {
-//     currentIndex++;
-//   } else {
-//     currentIndex = 0; 
-//   }
-//   updateSlidePosition();
-// }
-
-
-// function startAutoSlide() {
-//   intervalId = setInterval(nextSlide, 4000); 
-// }
-
-
-// function stopAutoSlide() {
-//   clearInterval(intervalId);
-// }
-
-
-// startAutoSlide();
-
-
-// document.querySelector('.prev-btn').addEventListener('click', () => {
-//   if (currentIndex > 0) {
-//     currentIndex--;
-//   } else {
-//     currentIndex = slides.children.length - 1; 
-//   }
-//   updateSlidePosition();
-//   stopAutoSlide(); 
-// });
-
-// document.querySelector('.next-btn').addEventListener('click', () => {
-//   nextSlide();
-//   stopAutoSlide(); 
-// });
-
-
-// function updateSlidePosition() {
-//   slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-// }
-
-
-// slider.addEventListener('mouseenter', stopAutoSlide);
-// slider.addEventListener('mouseleave', startAutoSlide);
-
-
 let slideIndex = 1;
 let slideInterval;
 
@@ -100,7 +45,7 @@ function autoSlide() {
     plusSlides(1);
 }
 
-slideInterval = setInterval(autoSlide, 3000);
+// slideInterval = setInterval(autoSlide, 3000);
 
 // Остановить автоматическое переключение при наведении на слайдер
 document.querySelector('.slideshow-container').addEventListener('mouseenter', function() {
